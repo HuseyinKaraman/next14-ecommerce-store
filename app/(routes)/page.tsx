8c6f2f3ce2eaf06b6099024f28f30ab18383fdd3
Hercules <hsy.krmn@gmail.com>
2024-03-21 02:28:20 +0300

    adding billboard

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,5 +1,20 @@
-const HomePage = () => {
-  return <div className="">Hello Store</div>;
+import Container from "@/components/ui/container";
+import Billboard from "@/components/billboard";
+import getBillboard from "@/actions/get-billboard";
+
+export const revalidate = 0; 
+
+const HomePage = async () => {
+
+  const billboard = await getBillboard("1eaca54b-608d-44ad-a012-23d607f28a74");
+
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard}/>
+      </div>
+    </Container>
+  )
 };
 
 export default HomePage;
