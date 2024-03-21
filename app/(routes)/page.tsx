54be91a9589594e75cc30dff9f3bdc0348dab335
Hercules <hsy.krmn@gmail.com>
2024-03-22 01:40:14 +0300

    Work On Featured Products

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,20 +1,25 @@
 import Container from "@/components/ui/container";
 import Billboard from "@/components/billboard";
 import getBillboard from "@/actions/get-billboard";
+import getProducts from "@/actions/get-products";
+import ProductList from "@/components/product-list";
 
-export const revalidate = 0; 
+export const revalidate = 0;
 
 const HomePage = async () => {
-
+  const products = await getProducts({ isFeatured: true });
   const billboard = await getBillboard("1eaca54b-608d-44ad-a012-23d607f28a74");
 
   return (
     <Container>
       <div className="space-y-10 pb-10">
-        <Billboard data={billboard}/>
+        <Billboard data={billboard} />
+        <div className="flex flex-col px-4 sm:px-6 lg:px-8">
+          <ProductList title="Featured Products" items={products} />
+        </div>
       </div>
     </Container>
-  )
+  );
 };
 
 export default HomePage;
