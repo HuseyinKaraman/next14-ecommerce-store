e9c7343ad2b272df4cee6e5078b31d964fe1a5a9
Hercules <hsy.krmn@gmail.com>
2024-03-25 01:05:42 +0300

    adding addtoCart func.

--- components/info.tsx
+++ components/info.tsx
@@ -6,12 +6,19 @@ import { Product } from "@/types";
 
 import Button from "@/components/ui/button";
 import Currency from "@/components/ui/currency";
+import useCart from "@/hooks/use-cart";
 
 interface InfoProps {
   data: Product;
 }
 
 const Info: React.FC<InfoProps> = ({ data }) => {
+  const cart = useCart();
+
+  const onAddToCart = () => {
+    cart.addItem(data);
+  };
+
   return (
     <div>
       <h1 className="text-3xl font-bold text-gray-900">{data?.name}</h1>
@@ -35,7 +42,7 @@ const Info: React.FC<InfoProps> = ({ data }) => {
         </div>
       </div>
       <div className="mt-10 flex items-center gap-x-3">
-        <Button className="flex items-center gap-x-2" >
+        <Button className="flex items-center gap-x-2" onClick={onAddToCart}>
           Add To Cart
           <ShoppingCart size={20} />
         </Button>
